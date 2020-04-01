72d0d820e21aab92b2e70df1e6e9dba11a41c46e
Keturah Howard <keturah.dev@gmail.com>
2020-04-01 15:23:13 -0700

    map out Posts crud functionality within controller, models, and apihelper.
    Co-authored-by: Jamison Cozart <jamisoncozart@gmail.com>
    >     Co-authored-by: Dusty McCord <dustymccord@gmail.com>
    >     Co-authored-by: Zakkrey Short <zakkreyjshort@gmail.com>

--- CatBoardInterface/Models/ApiHelper.cs
+++ CatBoardInterface/Models/ApiHelper.cs
@@ -21,6 +21,13 @@ namespace CatBoardInterface.Models
       var response = await client.ExecuteTaskAsync(request);
       return response.Content;
     }
+    public static async Task<string> GetPost(int boardId, int postId)
+    {
+      RestClient client = new RestClient("http://localhost:5000/api/");
+      RestRequest request = new RestRequest($"boards/{boardId}/posts/{postId}", Method.GET);
+      var response = await client.ExecuteTaskAsync(request);
+      return response.Content;
+    }
 
     public static async Task Post(string newBoard)
     {
@@ -28,6 +35,15 @@ namespace CatBoardInterface.Models
       RestRequest request = new RestRequest($"boards", Method.POST);
       request.AddHeader("Content-Type", "application/json");
       request.AddJsonBody(newBoard);
+      var response = await client.ExecuteTaskAsync(request);
+    }
+
+    public static async Task PostCreate(int boardId, string newPost)
+    {
+      RestClient client = new RestClient("http://localhost:5000/api/");
+      RestRequest request = new RestRequest($"boards/{boardId}", Method.POST);
+      request.AddHeader("Content-Type", "application/json");
+      request.AddJsonBody(newPost);
       var response = await client.ExecuteTaskAsync(request);
     }
 
@@ -39,11 +55,26 @@ namespace CatBoardInterface.Models
       request.AddJsonBody(newBoard);
       var response = await client.ExecuteTaskAsync(request);
     }
+    public static async Task PutPost(int boardId, int postId, string newPost)
+    {
+      RestClient client = new RestClient("http://localhost:5000/api/");
+      RestRequest request = new RestRequest($"boards/{boardId}/posts/{postId}", Method.PUT);
+      request.AddHeader("Content-Type", "application/json");
+      request.AddJsonBody(newPost);
+      var response = await client.ExecuteTaskAsync(request);
+    }
 
     public static async Task Delete(int id)
     {
       RestClient client = new RestClient("http://localhost:5000/api");
       RestRequest request = new RestRequest($"boards/{id}", Method.DELETE);
+      request.AddHeader("Content-Type", "application/json");
+      var response = await client.ExecuteTaskAsync(request);
+    }
+    public static async Task DeletePost(int boardId, int postId)
+    {
+      RestClient client = new RestClient("http://localhost:5000/api");
+      RestRequest request = new RestRequest($"boards/{boardId}/posts/{postId}", Method.DELETE);
       request.AddHeader("Content-Type", "application/json");
       var response = await client.ExecuteTaskAsync(request);
     }
