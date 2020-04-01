5752404389448b9a238755fd9c1b92162666d6c5
Keturah Howard <keturah.dev@gmail.com>
2020-04-01 11:49:04 -0700

    create models and instance methods for Board, and post only s we can see all posts in board details.
    Co-authored-by: Jamison Cozart <jamisoncozart@gmail.com>
    Co-authored-by: Dusty McCord <dustymccord@gmail.com>
    Co-authored-by: Zakkrey Short <zakkreyjshort@gmail.com>

---
+++ CatBoardInterface/Models/Post.cs
@@ -0,0 +1,10 @@
+namespace CatBoardInterface.Models
+{
+  public class Post
+  {
+    public string Title { get; set; }
+    public string Body { get; set; }
+
+    public int score { get; set; }
+  }
+}
